1023ee6dedbbe49ad4e121dc37721cac62c6b1b6
Sean Ballug <98096573+seanballug@users.noreply.github.com>
2026-04-20 12:07:12 +0800

    Generated, components, services, authguard

---
+++ src/app/pages/suppliers-list/suppliers-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suppliers-list',
+  imports: [],
+  templateUrl: './suppliers-list.html',
+  styleUrl: './suppliers-list.css',
+})
+export class SuppliersList {}
